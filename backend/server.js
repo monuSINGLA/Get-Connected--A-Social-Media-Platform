35abd7412ff2ea91ad5f4d411f09e35943af7f44
Yogesh Singla <monusingla104@gmail.com>
2024-05-27 22:14:39 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,52 @@
-import express from 'express'
-import donenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import {v2 as cloudinary} from 'cloudinary'
+import express from "express";
+import donenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+import path, { dirname } from "path";
 
-import authRoutes from './routes/auth.routes.js'
-import userRoutes from './routes/user.routes.js'
-import postRoutes from './routes/post.routes.js'
-import notificationRoutes from './routes/notification.routes.js'
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
-import connectMongoDB from './db/connectMongoDB.js'
+import connectMongoDB from "./db/connectMongoDB.js";
+import { fileURLToPath } from "url";
 
-donenv.config()
+donenv.config();
 
 //Cloudinary Configuration
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const app = express()
-const PORT = process.env.PORT || 8000
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.use(express.json({limit: "5mb"}))
-app.use(express.urlencoded({extended:true}))
+const app = express();
+const PORT = process.env.PORT || 8000;
+// const _dirname = path.resolve()
 
-app.use(cookieParser())
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 // routes
-app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/users", userRoutes)
-app.use("/api/v1/posts", postRoutes)
-app.use("/api/v1/notifications", notificationRoutes)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/notifications", notificationRoutes);
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on port : ${PORT}`)
-    connectMongoDB()
-})
+const frontendPath = path.resolve(__dirname, "../frontend/dist");
+app.use(express.static(frontendPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port : ${PORT}`);
+  connectMongoDB();
+});
